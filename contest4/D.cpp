0e5eb8e1656ea48f62e99c21b009b5d75af575c8
InversionSpaces <InversionSpaces@vivaldi.net>
2020-04-05 15:57:24 +0300

    D on sparse table

--- contest4/D.cpp
+++ contest4/D.cpp
@@ -1,138 +1,92 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <cstdio>
-#include <fstream>
 
 using namespace std;
 
-typedef int ll;
+size_t log2i(size_t k) {
+	size_t res = 0;
+	
+	while (k > 1) {
+		k >>= 1;
+		++res;
+	}
 
-class BITree {
+	return res;
+}
 
-#define FFUNC(i) (i & (i + 1))
-#define GFUNC(i) (i | (i + 1))
-
+class SparseTable {
 private:
-	vector<ll> inarr;
-	vector<ll> forward;
-	vector<ll> backward;
+	vector<vector<int>> table;
+	vector<int> arr;
+	vector<int> len2k;
 public:
-	BITree(const vector<ll>&& arr) : inarr(arr)
-	{
-		forward.resize(inarr.size());
-		backward.resize(inarr.size());
-
-		for (ll i = 0; i < backward.size(); ++i) {
-			ll tmax = i;
-			for (ll j = FFUNC(i); j < i; ++j)
-				tmax = inarr[tmax] > inarr[j] ? tmax : j;
-			backward[i] = tmax;
-		}
-
-		for (ll i = 0; i < forward.size(); ++i) {
-			ll tmax = i;
-			for (ll j = i + 1; j < inarr.size() && j <= GFUNC(i); ++j)
-				tmax = inarr[tmax] > inarr[j] ? tmax : j;
-			forward[i] = tmax;
-		}
-	}
-
-	ll segment(ll l, ll r) {
-		ll tmax = l;
-		ll vmax = inarr[l];
+	SparseTable(vector<int>&& tmp) : arr(tmp) {
+		const size_t& maxk = log2i(arr.size()) + 1;
 		
-		while (r >= 0 && FFUNC(r) >= l) {
-			const ll& indx = backward[r];
-			const ll& val = inarr[indx];
-			
-			if (vmax < val) {
-				tmax = indx;
-				vmax = val;
-			}
-			
-			r = FFUNC(r) - 1;
+		table.resize(arr.size());
+		for (size_t i = 0; i < table.size(); ++i) {
+			auto& v = table[i];
+			v.resize(maxk + 1);
+			v[0] = i;
 		}
 
-		while (GFUNC(l) <= r) {
-			const ll& indx = forward[l];
-			const ll& val = inarr[indx];
-			
-			if (vmax < val) {
-				tmax = indx;
-				vmax = val;
+		for (size_t k = 1, prevpow = 1; k < maxk; ++k, prevpow *= 2)
+			for (size_t i = 0; i < table.size(); ++i) {
+				const size_t& j = i + prevpow;
+				const size_t& maxl = table[i][k - 1];
+				const size_t& maxr = j < table.size() ? table[j][k - 1] : maxl;
+				table[i][k] = arr[maxl] > arr[maxr] ? maxl : maxr;
 			}
-			
-			l = GFUNC(l) + 1;
-		}
 
-		return tmax;
+		len2k.resize(arr.size());
+		for (size_t l = 0, powk = 1, k = 0; l < len2k.size(); ++l) {
+			if ((powk << 1) < l + 1)
+				powk <<= 1, ++k;
+			len2k[l] = k;
+		}
 	}
 
-#undef FFUNC
-#undef GFUNC
+	size_t segment(size_t l, size_t r) {
+		if (l == r) return l;
 
+		//cout << l << " to " << r << ":" << endl;
+		const size_t& len = r - l;
+		const size_t& k = len2k.at(len);
+		//cout << "k: " << k << endl;
+		const size_t& powk = 1 << k;
+		//cout << "powk: " << powk << endl;
+
+		const size_t& maxl = table.at(l).at(k);
+		const size_t& maxr = table.at(r - powk + 1).at(k);
+
+		//cout << "maxl: " << maxl << "; maxr: " << maxr << endl;
+
+		return arr.at(maxl) > arr.at(maxr) ? maxl : maxr;
+	}
 };
 
-#include <random>
-#include <cassert>
-
 int main() {
-	/*
-	while (true) {
-		cout << "-----------------" << endl;
-
-		ll n = rand() % 200 + 10;
-		cout << n << endl;
-		vector<ll> arr(n);
-		for (ll i = 0 ; i < n; ++i) {
-			arr[i] = rand() % 500;
-			cout << "[" << i << "]" << arr[i] << " ";
-		}
-		cout << endl;
-		
-		auto tmp = arr;
-		BITree tree(move(tmp));
-
-		ll l = rand() % n;
-		ll r = l + rand() % (n - l);
-
-		ll idx = l;
-		for (ll i = l + 1; i <= r; ++i)
-			if (arr[i] > arr[idx])
-				idx = i;
-
-		ll ans = tree.segment(l, r);
-		if (arr[idx] > arr[ans]) {
-			cout << l << " - " << r << endl;
-			cout << ans << " | " << idx << endl;
-			return 1;
-		}
-	}
-	*/
-
 	freopen("index-max.in", "r", stdin);
 	freopen("index-max.out", "w", stdout);
 
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
-
-	ll n;
+	
+	size_t n;
 	cin >> n;
 	
-	vector<ll> arr(n);
-	for (auto& i: arr) {
+	vector<int> arr(n);
+	for (auto& i: arr)
 		cin >> i;
-	}
 
-	BITree tree(move(arr));
-	
-	ll k;
+	SparseTable table(move(arr));
+
+	size_t k;
 	cin >> k;
-
 	while (k--) {
-		ll l, r;
+		size_t l, r;
 		cin >> l >> r;
-		cout << tree.segment(l - 1, r - 1) + 1 << '\n';
+		--l; --r;
+		cout << table.segment(l, r) + 1 << '\n';
 	}
 }
